1555e6df8a616c099d2076e73062ebff4b8c71b9
Rohit Singh <rohitsinghnegi910@gmail.com>
2025-09-22 16:48:40 +0530

    edit import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import About from "./components/About"
 import Contact from "./components/Contact"
 import Hero from "./components/Hero"
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
 import './index.css'
